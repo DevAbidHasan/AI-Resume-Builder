82484c0c2f42888dc45e9f4135586138cfb9f91d
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-10-11 12:51:06 +0600

    added features and testimonials sections using prebuild UI library component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../components/Home/Banner';
 import Hero from '../components/Home/Hero';
+import Features from '../components/Home/Features';
+import Testimonial from '../components/Home/Testimonial';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <Hero></Hero>
+           <Features></Features>
+           <Testimonial></Testimonial>
         </div>
     );
 };
